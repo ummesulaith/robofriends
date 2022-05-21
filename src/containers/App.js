9b6a6ba8d0ac034055eb07919afe400adc227507
Umme Suleyath <ummesul76@gmail.com>
2022-05-21 16:26:20 +0530

    Refactor: code clean up

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,10 +6,8 @@ import React, { Component } from "react";
 import './App.css';
 import Scroll from "../component/Scroll";
 
-
-
 class App extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             robots: [],
@@ -17,41 +15,41 @@ class App extends Component {
         }
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').then(response =>{
-          return  response.json();
-        }).then(users=>{
-            this.setState({robots:users})
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        }).then(users => {
+            this.setState({ robots: users })
         })
-        
+
         console.log('componentDidMount check')
     }
 
-    onSearchChange =(event)=>{
-        this.setState({searchField: event.target.value})
-        
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const { robots,searchField} = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
         console.log(filteredRobots)
-        if(this.state.robots.length === 0){
+        if (!robots.length) {
             return <h1>Loading</h1>
         }
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox  searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                <CardList robots={filteredRobots} />
+                    <CardList robots={filteredRobots} />
                 </Scroll>
-               
             </div>
 
         );
     }
-    
+
 
 }
 
